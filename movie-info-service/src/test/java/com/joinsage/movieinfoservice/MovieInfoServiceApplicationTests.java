342830d97df052ce613ae7d9db789c40e39fc62b
Yashpandey4 <yashpandey4@gmail.com>
2020-07-21 19:27:47 +0530

    initial commit

---
+++ movie-info-service/src/test/java/com/joinsage/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joinsage.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
