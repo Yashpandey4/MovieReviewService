83d25d639c8006ee738d91c4f6dc9798b528e160
pratyush.pandey <pratyush.pandey@joinsage.com>
2020-07-23 01:02:22 +0530

    Added Eureka Server for client discovery

--- movie-catalog-service/src/main/java/com/joinsage/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/joinsage/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,10 +2,13 @@ package com.joinsage.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServiceApplication {
 
 	@Bean
@@ -13,6 +16,11 @@ public class MovieCatalogServiceApplication {
 		// Instantiating RestTemplates on every API calls is a waste as we can do with just one instance.
 		// So we use a bean to share a single instance across classes.
 		return new RestTemplate();
+	}
+
+	@Bean
+	public WebClient.Builder getWebClientBuilder() {
+		return WebClient.builder();
 	}
 
 	public static void main(String[] args) {
--- movie-catalog-service/src/main/java/com/joinsage/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/joinsage/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,12 +3,15 @@ package com.joinsage.moviecatalogservice.resources;
 import com.joinsage.moviecatalogservice.models.CatalogItem;
 import com.joinsage.moviecatalogservice.models.Movie;
 import com.joinsage.moviecatalogservice.models.Rating;
+import com.joinsage.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,18 +23,29 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         // get rated movie ID - for each ID, call info service and get details. Finally put them all together.
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating(userId, "12", 4),
-                new Rating(userId, "11", 5)
-        );
+        UserRating ratings = restTemplate.getForObject("http://localhost:8082/ratings/users/" + userId, UserRating.class);
 
-        return ratings.stream().map(rating -> {
-            // Call Movie-Info service running on port 8081
+        return ratings.getUserRating().stream().map(rating -> {
+            // RestTemplate based Implementation: Call Movie-Info service running on port 8081
             Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieID(), Movie.class);
+
+            /*
+            // WebClient based implementation: Reactive Web Way (Call Movie-Info service running on port 8081)
+            Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8081/movies/" + rating.getMovieID())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+             */
+            // Put them all together
             return new CatalogItem(movie.getName(), "Desc", rating.getRating());
         })
                 .collect(Collectors.toList());
