34cb025b4951473047f7912b5426755814b3e56e
Mudreka Arif <mudreka.arif@joinsage.com>
2020-07-23 13:45:08 +0530

    Issues resolved

--- ratings-data-service/src/main/java/com/joinsage/ratingsdataservice/dao/MovieRatingDao.java
+++ ratings-data-service/src/main/java/com/joinsage/ratingsdataservice/dao/MovieRatingDao.java
@@ -7,25 +7,30 @@ import java.util.*;
 
 @Repository
 public class MovieRatingDao {
-//    private static Map<Integer, movieRating> Ratings;
     private static Map<Integer, Set<MovieRating>> Ratings;
     static {
         Ratings = new HashMap<Integer, Set<MovieRating>>();
+
         Set<MovieRating> user1 = new HashSet<>();
         user1.add(new MovieRating(1, 1, 5));
         user1.add(new MovieRating(1, 2, 3));
+
         Set<MovieRating> user2 = new HashSet<>();
         user2.add(new MovieRating(2, 3, 3));
         user2.add(new MovieRating(2, 2, 2));
+
         Ratings.put(1,user1);
         Ratings.put(2,user2);
     }
+
     public Collection<Set<MovieRating>> getAllRatings(){
         return this.Ratings.values();
     }
+
     public Set<MovieRating> getRatingsById(int id){
         return this.Ratings.get(id);
     }
+
     public void insertUser(MovieRating movieRating) {
         Set<MovieRating> newUser = new HashSet<>();
         newUser.add(movieRating);
