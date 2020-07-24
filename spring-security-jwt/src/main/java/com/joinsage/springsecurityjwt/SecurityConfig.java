184014712b6f26faed191f327edeb0cd1bbdceb6
Yashpandey4 <yashpandey4@gmail.com>
2020-07-24 16:59:05 +0530

    Added jwt tokens service

---
+++ spring-security-jwt/src/main/java/com/joinsage/springsecurityjwt/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.joinsage.springsecurityjwt;
+
+import com.joinsage.springsecurityjwt.services.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myUserDetailsService);
+        auth.inMemoryAuthentication()
+                .withUser("Pratyush").password("password").roles("USER")
+                .and()
+                .withUser("Uneet").password("password").roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/", "static/css", "static/js").permitAll()
+                .and().formLogin();  // default config
+    }
+}
