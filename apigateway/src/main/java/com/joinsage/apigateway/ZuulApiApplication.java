b36e367cc417c6348fbf19278b0d420d6dd9df8e
Yashpandey4 <yashpandey4@gmail.com>
2020-07-25 13:36:46 +0530

    Fixed Zuul Configs and added confiuration in Zuul Server

--- apigateway/src/main/java/com/joinsage/apigateway/ZuulApiApplication.java
+++ apigateway/src/main/java/com/joinsage/apigateway/ZuulApiApplication.java
@@ -4,14 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
+@Configuration
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-public class ZuulApiApplication {
+public class ZuulApiApplication extends ResourceServerConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApiApplication.class, args);
     }
 
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/security-jwt/**")
+                .permitAll()
+                .antMatchers("/**")
+                .authenticated();
+    }
 }
