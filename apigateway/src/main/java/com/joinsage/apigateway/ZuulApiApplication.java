57cf1a74d585d34109058bdee19632e3d16915f2
jainammm <jainam.shah@joinsage.com>
2020-07-28 01:56:49 +0530

    merge zuul and jwt security services

--- apigateway/src/main/java/com/joinsage/apigateway/ZuulApiApplication.java
+++ apigateway/src/main/java/com/joinsage/apigateway/ZuulApiApplication.java
@@ -4,26 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
-@Configuration
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-public class ZuulApiApplication extends ResourceServerConfigurerAdapter {
+public class ZuulApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApiApplication.class, args);
     }
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/security-jwt/**")
-                .permitAll()
-                .antMatchers("/**")
-                .authenticated();
-    }
 }
